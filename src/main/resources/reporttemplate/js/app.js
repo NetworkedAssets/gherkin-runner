087909f2a395044cb6f7ef6d0ac17f691836e4e3
Tomasz Łazarski <tomasz.lazarski@gmail.com>
2018-02-16 01:25:48 +0100

    New style of report layout

--- src/main/resources/reporttemplate/js/app.js
+++ src/main/resources/reporttemplate/js/app.js
@@ -1,19 +1,32 @@
-Vue.component('callback', {
+var callbackComponent = {
     props: ['report', 'title'],
-    template: '#callback-template'
-});
+    template: '#callback-template',
+    methods: {
+        showLogs: function () {
+            this.$emit('showlogs');
+        }
+    }
+};
 
-Vue.component('error', {
+var callbackRowComponent = {
+    props: ['report', 'title'],
+    template: '#callback-row-template',
+    methods: {
+        showLogs: function () {
+            this.$emit('showlogs');
+        },
+        showError: function () {
+            this.$emit('showerror');
+        }
+    }
+};
+
+var errorComponent = {
     props: ['report'],
     template: '#error-template'
-});
+};
 
-Vue.component('log', {
-    props: ['logs'],
-    template: '#log-template'
-});
-
-Vue.component('chart', {
+var chartComponent = {
     props: ['title', 'statistics', 'icon'],
     template: '#chart-template',
     mounted: function () {
@@ -26,35 +39,136 @@ Vue.component('chart', {
             this.statistics.multipleImplementatmions
         ]));
     }
-});
+};
+
+var stepRowComponent = {
+    props: ['stepreport'],
+    template: '#step-row-template',
+    methods: {
+        showLogs: function () {
+            this.$emit('showlogs');
+        },
+        showError: function () {
+            this.$emit('showerror');
+        }
+    }
+};
+
+var featureStateComponent = {
+    props: ['state'],
+    template: '#feature-state-template',
+    data: function () {
+        return {
+            color: this.stateToColor(this.state),
+            text: this.stateToText(this.state)
+        }
+    },
+    methods: {
+        stateToColor: function (state) {
+            switch(state) {
+                case "PASSED": return 'green';
+                case "FAILED": return 'red';
+                case "SKIPPED": return 'blue';
+                case "NOT_IMPLEMENTED": return 'grey';
+                case "MULTIPLE_IMPLEMENTATIONS": return 'yellow';
+            }
+        },
+        stateToText: function (state) {
+            switch(state) {
+                case "PASSED": return 'Passed';
+                case "FAILED": return 'Failed';
+                case "SKIPPED": return 'Skipped';
+                case "NOT_IMPLEMENTED": return 'Not implemented';
+                case "MULTIPLE_IMPLEMENTATIONS": return 'Multiple implementations';
+            }
+        }
+    }
+};
+
 
 new Vue({
     el: '#app',
-    data: function data() {
+    data: function () {
         return {
             report: reportData,
             suiteName: reportData.suiteName,
             environment: reportData.environment,
             passed: reportData.state === "PASSED",
-            startTime: moment(reportData.startTime).format("DD-MM-YYYY HH:mm:ss"),
+            startDate: moment(reportData.startTime).format("DD-MM-YYYY"),
+            startTime: moment(reportData.startTime).format("HH:mm:ss"),
             executionTime: humanizeExecutionTime(reportData.executionTime),
-            endTime: moment(reportData.endTime).format("DD-MM-YYYY HH:mm:ss")
+            endDate: moment(reportData.endTime).format("DD-MM-YYYY"),
+            endTime: moment(reportData.endTime).format("HH:mm:ss"),
+            flat: false,
+            currentReport: null
         }
+    },
+    methods: {
+        showLogs: function (report) {
+            this.currentReport = report;
+            $('#logs-modal').modal('show');
+        },
+        showError: function (report) {
+            this.currentReport = report;
+            $('#error-modal').modal('show');
+        }
+    },
+    updated: function () {
+        $('#logs-modal').modal('refresh');
+        $('#error-modal').modal('refresh');
     },
     mounted: function () {
         document.title = reportData.suiteName + " Report";
 
-        $('.features-accordion, #logs .accordion').accordion({exclusive: false, animateChildren: false});
+        $('.feature-accordion, #logs .accordion').accordion({exclusive: false, animateChildren: false});
+        $('.ui.accordion.feature-accordion > .title, .ui.accordion.scenario-accordion > .title, #logs .ui.accordion > .title').click()
 
-        var envButton = $('#environment').find('.massive.button');
+        var envButton = $('.environment-button');
         envButton.click(function () {
             envButton.toggleClass("active");
             $('#endpoints').slideToggle();
         });
 
-        $('.smooth-goto').on('click', function () {
-            $('html, body').animate({scrollTop: $(this.hash).offset().top - 50}, 500);
-            return false;
+        $('.smooth-goto').on('click', function (e) {
+            console.log("click")
+            $('html, body').animate({scrollTop: $(this.hash).offset().top - 99}, 500);
+            $('.ui.dropdown').dropdown('hide');
+            if (window.event) { // ie11
+                window.event.returnValue = false;
+                window.event.cancelBubble = true;
+            } else if (e) {
+                e.stopPropagation();
+                e.preventDefault();
+            }
         });
+
+        setTimeout(function () {
+            $('#preloader').fadeOut(1000);
+        }, 300)
+
+        $(window).on('scroll', function () {
+            var featuresSummary = $(".report-table");
+            var bottomOfFeaturesSummary = featuresSummary.offset().top + featuresSummary.outerHeight(true);
+            var featuresSection = $("#features");
+            var bottomOfFeaturesSection = featuresSection.offset().top + featuresSection.outerHeight(true);
+
+            var currentPosition = $(window).scrollTop();
+            var backToFeaturesSummaryButtton = $(".back-to-features-summary");
+            if(currentPosition > bottomOfFeaturesSummary && currentPosition < bottomOfFeaturesSection)
+                backToFeaturesSummaryButtton.fadeIn();
+            else
+                backToFeaturesSummaryButtton.fadeOut();
+
+        });
+
+        $('.ui.dropdown').dropdown();
+    },
+    components: {
+        'callback': callbackComponent,
+        'callbackrow': callbackRowComponent,
+        'error': errorComponent,
+        'chart': chartComponent,
+        'steprow': stepRowComponent,
+        'featurestate': featureStateComponent
     }
 });
--- src/main/resources/reporttemplate/js/utils.js
+++ src/main/resources/reporttemplate/js/utils.js
@@ -20,3 +20,39 @@ function getStyleClasses(element) {
         'multiple-implementations': element.state === 'MULTIPLE_IMPLEMENTATIONS'
     }
 }
+
+function processData(data) {
+    data.featureReports = data.featureReports.map(function(featureReport) {
+        featureReport.fullTree = [featureReport.feature.name];
+        if(featureReport.beforeReport) {
+            featureReport.beforeReport.fullTree = featureReport.fullTree.slice();
+            featureReport.beforeReport.fullTree.push("Before feature")
+        }
+        if(featureReport.afterReport) {
+            featureReport.afterReport.fullTree = featureReport.fullTree.slice();
+            featureReport.afterReport.fullTree.push("After feature");
+        }
+        featureReport.scenarioReports = featureReport.scenarioReports.map(function (scenarioReport) {
+            scenarioReport.fullTree = featureReport.fullTree.slice();
+            scenarioReport.fullTree.push(scenarioReport.scenario.name);
+            scenarioReport.stepReports = scenarioReport.stepReports.map(function (stepReport) {
+                stepReport.fullTree = scenarioReport.fullTree.slice();
+                stepReport.fullTree.push(stepReport.step.fullContent);
+                return stepReport
+            });
+            if(scenarioReport.beforeReport) {
+                scenarioReport.beforeReport.fullTree = scenarioReport.fullTree.slice();
+                scenarioReport.beforeReport.fullTree.push("Before scenario");
+            }
+            if(scenarioReport.afterReport) {
+                scenarioReport.afterReport.fullTree = scenarioReport.fullTree.slice();
+                scenarioReport.afterReport.fullTree.push("After scenario");
+            }
+            return scenarioReport
+        });
+        return featureReport
+    });
+    return data
+}
+
+reportData = processData(reportData);
