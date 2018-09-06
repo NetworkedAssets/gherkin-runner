368ad694ffc194d10cd13d6b31638c4bb290ff0f
Tomasz Łazarski <tomasz.lazarski@gmail.com>
2018-09-06 16:57:46 +0200

    Fix report for few scenarios with the same name

--- src/main/kotlin/com/networkedassets/gherkin/runner/report/JUnitReportExporter.kt
+++ src/main/kotlin/com/networkedassets/gherkin/runner/report/JUnitReportExporter.kt
@@ -26,42 +26,54 @@ class JUnitReportExporter : ReportExporter {
     }
 
     private fun convertFeatureReportToTestsuite(featureReport: FeatureReport) =
-        Testsuite().apply {
-            name = featureReport.feature.name
-            tests = featureReport.getNumberOfScenarios().toString()
-            skipped = (featureReport.getNumberOfSkippedScenarios() + featureReport.getNumberOfMultipleImplementationsScenarios()
-                    + featureReport.getNumberOfNotImplementedScenarios()).toString()
-            failures = featureReport.getNumberOfFailedScenarios().toString()
-            errors = "0"
-            timestamp = featureReport.startTime?.format(DateTimeFormatter.ISO_DATE_TIME)
-            hostname = InetAddress.getLocalHost().hostName
-            time = (featureReport.executionTime / 1000.0).toString()
-            properties = Properties()
-            systemOut = featureReport.log.joinToString("\n") { it.message }
-            systemErr = ""
-            val testcases = featureReport.scenarioReports.map { convertScenarioReportToTestcase(featureReport.feature.name, it) }
-            testcase.addAll(testcases)
-        }
+            Testsuite().apply {
+                name = featureReport.feature.name
+                tests = featureReport.getNumberOfScenarios().toString()
+                skipped = (featureReport.getNumberOfSkippedScenarios() + featureReport.getNumberOfMultipleImplementationsScenarios()
+                        + featureReport.getNumberOfNotImplementedScenarios()).toString()
+                failures = featureReport.getNumberOfFailedScenarios().toString()
+                errors = "0"
+                timestamp = featureReport.startTime?.format(DateTimeFormatter.ISO_DATE_TIME)
+                hostname = InetAddress.getLocalHost().hostName
+                time = (featureReport.executionTime / 1000.0).toString()
+                properties = Properties()
+                systemOut = featureReport.log.joinToString("\n") { it.message }
+                systemErr = ""
+                val repeatedScenarios = featureReport.scenarioReports.groupBy { it.scenario.name }.filter { it.value.size > 1 }
+                val notRepeatedScenarios = featureReport.scenarioReports.groupBy { it.scenario.name }.filter { it.value.size == 1 }
+                val testcases = repeatedScenarios.flatMap {
+                    it.value.mapIndexed { i, scenarioReport ->
+                        convertScenarioReportToTestcase(featureReport.feature.name, "${scenarioReport.scenario.name} #${i + 1}", scenarioReport)
+                    }
+                }.union(
+                        notRepeatedScenarios.flatMap {
+                            it.value.map {
+                                convertScenarioReportToTestcase(featureReport.feature.name, it.scenario.name, it)
+                            }
+                        }
+                )
+                testcase.addAll(testcases)
+            }
 
-    private fun convertScenarioReportToTestcase(featureName: String, scenarioReport: ScenarioReport) =
-        Testcase().apply {
-            name = scenarioReport.scenario.name
-            classname = featureName
-            time = (scenarioReport.executionTime / 1000.0).toString()
-            val failures = scenarioReport.stepReports.filter { scenarioReport.state == ReportState.FAILED }.map {
-                Failure().apply {
-                    message = it.getError()
-                    type = it.thrownException?.let { it::class.qualifiedName }
-                    content = it.getErrorStackTrace()
+    private fun convertScenarioReportToTestcase(featureName: String, scenarioName: String, scenarioReport: ScenarioReport) =
+            Testcase().apply {
+                name = scenarioName
+                classname = featureName
+                time = (scenarioReport.executionTime / 1000.0).toString()
+                val failures = scenarioReport.stepReports.filter { scenarioReport.state == ReportState.FAILED }.map {
+                    Failure().apply {
+                        message = it.getError()
+                        type = it.thrownException?.let { it::class.qualifiedName }
+                        content = it.getErrorStackTrace()
+                    }
+                }
+                failure.addAll(failures)
+                val skippedStates = listOf(ReportState.NOT_IMPLEMENTED, ReportState.SKIPPED, ReportState.MULTIPLE_IMPLEMENTATIONS,
+                        ReportState.MULTIPLE_IMPLEMENTATIONS)
+                if (scenarioReport.state in skippedStates) {
+                    skipped = ""
                 }
             }
-            failure.addAll(failures)
-            val skippedStates = listOf(ReportState.NOT_IMPLEMENTED, ReportState.SKIPPED, ReportState.MULTIPLE_IMPLEMENTATIONS,
-                    ReportState.MULTIPLE_IMPLEMENTATIONS)
-            if(scenarioReport.state in skippedStates) {
-                skipped = ""
-            }
-        }
 
     private fun testsuiteToXMLString(testsuite: Testsuite): String {
         val jaxbContext = JAXBContext.newInstance(Testsuite::class.java)
