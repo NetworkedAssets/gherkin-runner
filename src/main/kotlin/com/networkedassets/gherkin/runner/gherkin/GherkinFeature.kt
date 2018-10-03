2abb32645bd1964464b8eb07a375c086d9a603e7
YevheniiHladkevych <yhladkevych@networkedassets.org>
2018-10-03 11:01:23 +0200

    Added parsing GIVEN on feature scope
    Signed-off-by: YevheniiHladkevych <yhladkevych@networkedassets.org>

--- src/main/kotlin/com/networkedassets/gherkin/runner/gherkin/GherkinFeature.kt
+++ src/main/kotlin/com/networkedassets/gherkin/runner/gherkin/GherkinFeature.kt
@@ -5,6 +5,7 @@ import java.io.Serializable
 
 data class GherkinFeature(
         val name: String,
+        val givenProps: GherkinStep,
         val tags: List<String>) : Serializable {
     @JsonIgnore
     val scenarios = mutableListOf<GherkinScenario>()
