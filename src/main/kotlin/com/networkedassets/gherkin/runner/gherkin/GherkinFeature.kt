91e90905b46b22e2a5fc53e3effb8e8947dfc89c
YevheniiHladkevych <yhladkevych@networkedassets.org>
2018-10-08 09:00:38 +0200

    Fix the build
    Signed-off-by: YevheniiHladkevych <yhladkevych@networkedassets.org>

--- src/main/kotlin/com/networkedassets/gherkin/runner/gherkin/GherkinFeature.kt
+++ src/main/kotlin/com/networkedassets/gherkin/runner/gherkin/GherkinFeature.kt
@@ -1,12 +1,10 @@
 package com.networkedassets.gherkin.runner.gherkin
 
 import com.fasterxml.jackson.annotation.JsonIgnore
-import gherkin.ast.Background
 import java.io.Serializable
 
 data class GherkinFeature(
         val name: String,
-        val background: Background,
         val tags: List<String>) : Serializable {
     @JsonIgnore
     val scenarios = mutableListOf<GherkinScenario>()
