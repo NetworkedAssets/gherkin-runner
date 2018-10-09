03cae1f43cbc93ec965ba3e967cca09b9e50697e
YevheniiHladkevych <yhladkevych@networkedassets.org>
2018-10-09 11:06:57 +0200

    Add utility methods

--- src/main/kotlin/com/networkedassets/gherkin/runner/gherkin/GherkinBackground.kt
+++ src/main/kotlin/com/networkedassets/gherkin/runner/gherkin/GherkinBackground.kt
@@ -1,5 +1,40 @@
 package com.networkedassets.gherkin.runner.gherkin
 
 import gherkin.ast.Background
+import gherkin.ast.DataTable
+import gherkin.ast.Step
 
-data class GherkinBackground(val background: List<Background>)
+class GherkinBackground(val background: List<Background>) {
+    fun getSteps(): List<Step> {
+        return background.map { back -> back.steps }.flatten()
+    }
+
+    fun getStep(stepNum: Int): Step? {
+        return background.map { back -> back.steps }.flatten().getOrNull(stepNum)
+    }
+
+    fun getDataTableForStep(stepNum: Int): Array<Array<String>>? {
+        if (!containsData(stepNum)) {
+            return null
+        }
+       return extractDataTable(getStep(stepNum))
+    }
+
+    fun containsData(stepNum: Int): Boolean {
+        if (getStep(stepNum)?.argument == null) {
+            return false
+        }
+        return true
+    }
+
+    fun getDataTableByStepName(stepName: String):  Array<Array<String>>? {
+        val step = background.map { back -> back.steps }.flatten().find { step -> step.text.equals(stepName) }
+        return extractDataTable(step)
+    }
+
+    private fun extractDataTable(step: Step?): Array<Array<String>>? {
+        val argument = step?.argument
+        val dataTable = argument as? DataTable
+        return dataTable?.rows?.map { it.cells.map { it.value }.toTypedArray() }?.toTypedArray()
+    }
+}
